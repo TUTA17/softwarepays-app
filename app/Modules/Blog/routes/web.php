aaa4061af2e4e23387d59568517195ee393bcd04
TUTA17 <anhtutran.tb97@gmail.com>
2026-07-18 20:47:58 +0700

    Add /guides/{slug} route alias for the Blog show page
    Lets guide posts be reached at /guides/{slug} in addition to the
    existing /tin-tuc/{slug}, same controller/view either way.

--- app/Modules/Blog/routes/web.php
+++ app/Modules/Blog/routes/web.php
@@ -7,5 +7,8 @@ Route::middleware(['web'])->group(function () {
     Route::get('/tin-tuc', [BlogController::class, 'index'])->name('blog.index');
     Route::get('/huong-dan', [BlogController::class, 'guides'])->name('blog.guides');
     Route::get('/tin-tuc/{slug}', [BlogController::class, 'show'])->name('blog.show');
+    // Alias cho các bài Hướng dẫn muốn URL /guides/{slug} thay vì /tin-tuc/{slug} mặc định
+    // (cùng controller/view, chỉ khác đường dẫn URL).
+    Route::get('/guides/{slug}', [BlogController::class, 'show'])->name('blog.show.guide');
     Route::get('/api/cron/fetch-news', [BlogController::class, 'fetchCron'])->name('blog.fetchCron');
 });
